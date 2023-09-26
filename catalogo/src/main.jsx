0a333d7e0c1d0a68a6314643461a1b99687ec4b6
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-09-26 17:04:59 -0300

    Update main.jsx

--- catalogo/src/main.jsx
+++ catalogo/src/main.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter as Routes, Router, Route, Link}
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
