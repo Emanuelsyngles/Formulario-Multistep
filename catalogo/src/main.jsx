87e3a2406bd63ae0b4ef3dc289dc44f22a4faf41
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-09-27 16:44:29 -0300

    Ajustando as rotas

--- catalogo/src/main.jsx
+++ catalogo/src/main.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import App from './App'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element ={<Home/>}></Route>
+          <Route path="movie/:id" element ={<Movie/>}></Route>
+          <Route path="search" element ={<Search/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
