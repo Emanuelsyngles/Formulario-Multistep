873bab18ec8d20615b90066342752f583a03685d
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-09-27 17:13:23 -0300

    Continuando modificações internas

--- catalogo/src/App.jsx
+++ catalogo/src/App.jsx
@@ -1,18 +1,15 @@
-import { Link, Outlet } from 'react-router-dom'
+import {Outlet } from 'react-router-dom'
 
 import './App.css'
+
+import Navbar from './componentes/Navbar'
+
 function App() {
 
   return (
     <>
       <div>
-        <nav id="navbar">
-          <h2>
-            <Link to="/">Catalogo de filmes</Link>
-          </h2>
-          <Link to="/movie/1">Movie</Link>
-          <Link to="/search">Search</Link>
-        </nav>
+        <Navbar />
         <h2>Movies Lib</h2>
         <Outlet/>
       </div>
