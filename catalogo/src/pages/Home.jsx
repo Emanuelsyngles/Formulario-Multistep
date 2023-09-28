1b046f76424e9fa57d94ad14eaa4123ad0c3b901
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-09-28 18:41:11 -0300

    update Home.jsx

--- catalogo/src/pages/Home.jsx
+++ catalogo/src/pages/Home.jsx
@@ -3,7 +3,18 @@ import { useState, useEffect } from 'react'
 const moviesURL = import.meta.env.VITE_API;
 const apikey = import.meta.env.VITE_API_KEY;
 
+
 const Home = () => {
+
+    const [topMovies, setTopMovies] = useState([])
+
+    const getTopRatedMovies = async(url) => {
+        const res = await fetch(url) 
+        const data = await res.json()
+
+        console.log(data)
+    };
+
     return ( 
         <div>Home</div>
     )
