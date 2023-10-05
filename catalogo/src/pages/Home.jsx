91bbeffc79f145b2891e1043e3dda9b03001391b
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-05 14:05:04 -0300

    Refazendo algumas partes

--- catalogo/src/pages/Home.jsx
+++ catalogo/src/pages/Home.jsx
@@ -1,33 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react"
 
 const moviesURL = import.meta.env.VITE_API;
-const apikey = import.meta.env.VITE_API_KEY;
-
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
-
-    const [topMovies, setTopMovies] = useState([])
-
-    const getTopRatedMovies = async (url) => {
-        const res = await fetch(url) 
-        const data = await res.json()
-
-        setTopMovies(data.results)
-    };
-
-    useEffect (() => {
-
-        const topRatedURL = `${moviesURL} top_rated? ${apikey}`
-
-        getTopRatedMovies(topRatedURL)
-        console.log(topRatedURL)
-    }, [])
-
-    return ( 
-        <div>
-            {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
-        </div>
-    )
+  return 
 }
 
 export default Home
