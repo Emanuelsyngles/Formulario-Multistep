d8b37a1e38bd75c53b4d47cfddee9e7e5c4298be
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-05 14:13:28 -0300

    Update Home.jsx

--- catalogo/src/pages/Home.jsx
+++ catalogo/src/pages/Home.jsx
@@ -4,6 +4,24 @@ const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
+
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+
+    const res = await fetch()
+    const data = await res.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+
+    const topratedURL = `${moviesURL}top_rated?${apiKey}`;
+
+    console.log(topratedURL);
+  }, [])
+
   return 
 }
 
