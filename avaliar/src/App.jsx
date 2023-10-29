a62e011012465cf5fba4f41d2754aa939745f760
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-28 21:22:46 -0300

    Update App.jsx

--- avaliar/src/App.jsx
+++ avaliar/src/App.jsx
@@ -3,7 +3,19 @@ import './App.css'
 const App = () => {
   return (
     <div className="App">
-      <h1>React Multistep</h1>
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto.</p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          <div className="action">
+            <button type="button">Voltar</button>
+            <button type="submit"></button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
