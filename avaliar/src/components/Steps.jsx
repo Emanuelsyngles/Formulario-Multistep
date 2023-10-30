0f98a1b32d630742130280b42b86ae07daea7ba4
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-30 17:31:38 -0300

    Configurando o css

--- avaliar/src/components/Steps.jsx
+++ avaliar/src/components/Steps.jsx
@@ -1,25 +1,25 @@
-import {AiOutlineUser, AiOutlineStar} from 'react-icons/ai'
-import {FiSend} from 'react-icons/fi'
+import { AiOutlineUser, AiOutlineStar } from 'react-icons/ai'
+import { FiSend } from 'react-icons/fi'
 
 import "./Steps.css"
 
-const Steps = (currentStep) => {
-  return (
-    <div className="steps">
-      <div className="step">
-        <AiOutlineUser/>
-        <p>identificação</p>
-      </div>
-      <div className="step">
-        <AiOutlineStar/>
-        <p>Avaliação</p>
-      </div>
-      <FiSend/>
-      <div className="step">
-        <p>Envio</p>
-      </div>
-    </div>
-  )
+const Steps = ({ currentStep }) => {
+    return (
+        <div className="steps">
+            <div className="step active">
+                <AiOutlineUser />
+                <p>Identificação</p>
+            </div>
+            <div className={`step ${currentStep >= 1 ? "active" : ""}`}>
+                <AiOutlineStar />
+                <p>Avaliação</p>
+            </div>
+            <div className={`step ${currentStep >= 2 ? "active" : ""}`}>
+                <FiSend />
+                <p>Envio</p>
+            </div>
+        </div>
+    )
 }
 
 export default Steps
