4f9b7e020f4525143db1acb2afdd1850e280da33
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-30 16:51:07 -0300

    Update Steps.jsx

--- avaliar/src/components/Steps.jsx
+++ avaliar/src/components/Steps.jsx
@@ -1,14 +1,20 @@
+import {AiOutlineUser, AiOutlineStar} from 'react-icons/ai'
+import {FiSend} from 'react-icons/fi'
+
 import "./Steps.css"
 
 const Steps = (currentStep) => {
   return (
     <div className="steps">
       <div className="step">
+        <AiOutlineUser/>
         <p>identificação</p>
       </div>
       <div className="step">
+        <AiOutlineStar/>
         <p>Avaliação</p>
       </div>
+      <FiSend/>
       <div className="step">
         <p>Envio</p>
       </div>
