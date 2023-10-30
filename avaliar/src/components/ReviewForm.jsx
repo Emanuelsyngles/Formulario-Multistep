ef621929deb07dabbd60113360fa9b210cd47bf9
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-30 17:59:30 -0300

    Update ReviewForm.jsx

--- avaliar/src/components/ReviewForm.jsx
+++ avaliar/src/components/ReviewForm.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+}from "react-icons/Bs"
 
 const ReviewForm = () => {
   return (
-    <div>
-      <h2>Review Form</h2>
+    <div className="review-form">
+      <div className="form-control score-container">
+        <label className="radio-container">
+          <input type="radio" value="unsatisfied" name="review" required />
+          <BsFillEmojiFrownFill />
+          <p>Insatisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" value="neutral" name="review" required />
+          <BsFillEmojiNeutralFill />
+          <p>Poderia ser melhor</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" value="satisfied" name="review" required />
+          <BsFillEmojiSmileFill />
+          <p>Satisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" value="very_satisfied" name="review" required />
+          <BsFillEmojiHeartEyesFill />
+          <p>Insatisfeito</p>
+        </label>
+      </div>
+      <div>
+        <div className="form-control">
+          <label htmlFor="comment">Comentário:</label>
+        </div>
+      </div>
     </div>
   )
 }
